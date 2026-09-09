26ae125d3fbf1f7c11b3e0892381f61bbbdba5d9
UHANGNAG <hhs095615@gmail.com>
2026-09-09 17:03:50 +0900

    feat: getGames(), getGame()
    게임 목록 조회와 게임 상세 조회
    게임 목록을 조회하기 위한 GameSummaryResponse DTO를 만들고 이를 가지고 GameService에서 getGames()를 구현

--- src/main/java/com/gamebasic/game/service/GameService.java
+++ src/main/java/com/gamebasic/game/service/GameService.java
@@ -2,6 +2,7 @@ package com.gamebasic.game.service;
 
 import com.gamebasic.game.dto.CreateRequest;
 import com.gamebasic.game.dto.GameDetailResponse;
+import com.gamebasic.game.dto.GameSummaryResponse;
 import com.gamebasic.game.dto.ProgressRequest;
 import com.gamebasic.game.entity.Game;
 import com.gamebasic.game.repository.GameRepository;
@@ -87,14 +88,45 @@ public class GameService {
     }
 
     // TODO (Lv 7): 게임 목록 조회. 주석을 풀고 구현하세요.
-    // @Transactional(readOnly = true)
-    // public List<GameSummaryResponse> getGames() {
-    // }
+     @Transactional(readOnly = true)
+     public List<GameSummaryResponse> getGames() {
+        List<Game> games = gameRepository.findAllByOrderByIdDesc();
+
+        return games.stream()
+                .map(game -> new GameSummaryResponse(
+                        game.getId(),
+                        game.getPlayerName(),
+                        game.getCurrentHp(),
+                        game.getCurrentFloor(),
+                        game.getPhase(),
+                        game.getStatus(),
+                        runCardRepository.findAllByGameOrderByIdAsc(game).size()
+                )).toList();
+     }
 
     // TODO (Lv 7): 게임 상세 조회. 주석을 풀고 구현하세요.
-    // @Transactional(readOnly = true)
-    // public GameDetailResponse getGame(Long gameId) {
-    // }
+     @Transactional(readOnly = true)
+     public GameDetailResponse getGame(Long gameId) {
+        Game game = gameRepository.findById(gameId).orElseThrow(
+                () -> new IllegalStateException("게임을 찾을 수 없습니다. id = {gameId}")
+        );
+
+        List<RunCard> cards = runCardRepository.findAllByGameOrderByIdAsc(game);
+        List<CardResponse> deck = new ArrayList<>();
+        for (RunCard card : cards) {
+            deck.add(new CardResponse(card.getId(), card.getCardType(), card.getAcquiredFloor()));
+        }
+
+        return new GameDetailResponse(
+                game.getId(),
+                game.getPlayerName(),
+                game.getCurrentHp(),
+                game.getCurrentFloor(),
+                game.getPhase(),
+                game.getStatus(),
+                deck
+            );
+     }
 
     // TODO (Lv 8): 플레이어 이름 변경 — 변경 감지로 수정
     // TODO (Lv 8): 게임 삭제
